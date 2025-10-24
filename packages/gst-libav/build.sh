b0c006e6acf7850e5050a1b8193bc11b7ab2541f
hdydgsgg58-svg <hdydgsgg58@gmail.com>
2025-10-24 22:29:39 +0300

    Delete packages directory@cosimo1110

--- packages/gst-libav/build.sh
+++
@@ -1,9 +0,0 @@
-TERMUX_PKG_HOMEPAGE=https://gstreamer.freedesktop.org/modules/gst-libav.html
-TERMUX_PKG_DESCRIPTION="GStreamer Libav plug-in contains one plugin with a set of elements using the Libav library code"
-TERMUX_PKG_LICENSE="LGPL-2.1"
-TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.26.7"
-TERMUX_PKG_SRCURL=https://gstreamer.freedesktop.org/src/gst-libav/gst-libav-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=91d4a7a14527d97fe0ce7ccedb3554e8f692b216429ef2b1850944bbf5435c09
-TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="ffmpeg, glib, gst-plugins-base, gstreamer"
